cdb3916686b589fcda5f508497107570e40a06e1
LikaTchikadze <likatchikadze1@gmail.com.ge>
2024-09-22 19:32:37 +0400

    first commit

---
+++ day 0/leason2.py
@@ -0,0 +1,19 @@
+name = ("lika")
+surname=("tchikadze")
+print(name)
+play=("volleyball")
+age=13
+heigh=149.5
+
+#
+
+print(name + " " + surname)
+
+#print(name + age)
+
+print(type(age))
+print(type(heigh))
+print(type(surname))
+print(type(play))
+
+print( "i_am" + " " +str(name)+" " + str(surname)+ "_i_am_" + str(heigh)+ "_tall" + "_and_i_play_" + str(play)+ "ty"+ "_:3")
